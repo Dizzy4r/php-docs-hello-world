4d168956595c346d4590b76a14a91a0c17fbe156
Dizzy4r <111083740+Dizzy4r@users.noreply.github.com>
2024-01-27 15:51:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -17,6 +17,12 @@ echo "Hello World!";
 echo "Hei no er eg e in live server lol";
 ?>
 
+    
+<?php
+$con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_real_connect($con, "tryingagain.mysql.database.azure.com", "dizzy4r93", "gjnfkitemt93-", "demo_db", 3306, MYSQLI_CLIENT_SSL);
+?>
 
     
 </body>
