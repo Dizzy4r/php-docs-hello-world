5de4af50fdb595324f46ea977ce5dbc5b591ffe9
Dizzy4r <111083740+Dizzy4r@users.noreply.github.com>
2024-01-27 13:53:23 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,11 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
     <title>Document</title>
 </head>
 <body>
 
 <h1> heiheiheiheiheihei</h1>
+
 <?php
 
 echo "Hello World!";
