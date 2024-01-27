9fb9f265657c47628dc2de86dee9b8187ae7aa16
Dizzy4r <111083740+Dizzy4r@users.noreply.github.com>
2024-01-27 13:33:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h1> heiheiheiheiheihei</h1>
 <?php
 
 echo "Hello World!";
 
-echo "Hei no er eg ein live server lol";
+echo "Hei no er eg e in live server lol";
 ?>
+
+
+    
+</body>
+</html>
+
