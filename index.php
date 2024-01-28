089df0c3d6b3a2971d1f3ce6858b39a3165734d5
Dizzy4r <111083740+Dizzy4r@users.noreply.github.com>
2024-01-28 16:46:00 +0100

    Update index.php

--- index.php
+++ index.php
@@ -20,11 +20,17 @@ $con = mysqli_init();
 mysqli_ssl_set($con,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, "tryingagain.mysql.database.azure.com", "dizzy4r93", "gjnfkitemt93-", "db_trying", 3306, MYSQLI_CLIENT_SSL);
 
+if($con) {
+    echo "Connection to database OK";
+}
+else {
+    echo "No DB connection";
+}
+
 ?>
 
 
-?>
-    
+
 </body>
 </html>
 
