2c6f2ed6b3aaceb0ab2ae2a56c154841b99e000e
Dizzy4r <111083740+Dizzy4r@users.noreply.github.com>
2024-01-29 21:59:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 echo "Im going crazy because php wont let me connect my database";
 
 $conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootG2.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, "tryingagain.mysql.database.azure.com", "dizzy4r93", "gjnfkitemt93-", "db_trying", 3306,  NULL, MYSQLI_CLIENT_SSL);
 
 if (mysqli_connect_errno())
