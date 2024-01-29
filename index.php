c903479d1340ffb353c84abf53b2277d2f4cb86b
Dizzy4r <111083740+Dizzy4r@users.noreply.github.com>
2024-01-29 21:16:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="index.css">
-    <title>Welcome to Trying-again</title>
+    <title>Whalecum to Trying-again</title>
 </head>
 <body>
 
