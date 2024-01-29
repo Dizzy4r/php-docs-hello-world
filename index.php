1c374f409e06aa3850c003b37fc0f1532bab840f
Dizzy4r <111083740+Dizzy4r@users.noreply.github.com>
2024-01-29 17:55:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,19 +13,22 @@
 
 $conn = mysqli_init();
 mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, "tryingagain.mysql.database.azure.com", "dizzy4r93", "gjnfkitemt93-", "db_trying", 3306, NULL, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($conn, "tryingagain.mysql.database.azure.com", "dizzy4r93", "gjnfkitemt93-", "db_trying", 3306, MYSQLI_CLIENT_SSL);
 
 if (mysqli_connect_errno())
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
+    echo "nicht gut";
 }
 else {
-    echo "Connection established.";
+    echo "<h1>Connection established.</h1>";
         }
 
 ?>
 
-<h1> Overskrift. </h1>
+<?php
+echo "HEI!!!";
+?>
 
 </body>
 </html>
