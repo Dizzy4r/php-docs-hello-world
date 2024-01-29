07c0e533594fb2f4295ac58d54c28b524b0fbf48
Dizzy4r <111083740+Dizzy4r@users.noreply.github.com>
2024-01-29 21:40:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -11,12 +11,15 @@
 <?php
 
 $conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, "tryingagain.mysql.database.azure.com", "dizzy4r93", "gjnfkitemt93-", "db_trying", 3306);
 
 if (mysqli_connect_errno())
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+else {
+    "nice db is online";
 }
 
 $query = "SELECT * FROM user";
