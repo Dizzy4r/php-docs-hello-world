b95d7f4488048f2fd72a312a0f983746dd19fb1f
Dizzy4r <111083740+Dizzy4r@users.noreply.github.com>
2024-01-29 21:10:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,8 @@
 <?php
 
 $conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+
 mysqli_real_connect($conn, "tryingagain.mysql.database.azure.com", "dizzy4r93", "gjnfkitemt93-", "db_trying", 3306, MYSQLI_CLIENT_SSL);
 
 
