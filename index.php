d6da564d8f686c405d59bc2b8872d7d9d8236df2
Dizzy4r <111083740+Dizzy4r@users.noreply.github.com>
2024-01-29 21:52:22 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,6 +12,18 @@
 
 echo "Im going crazy because php wont let me connect my database";
 
+$conn = mysqli_init();
+mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_real_connect($conn, "tryingagain.mysql.database.azure.com", "dizzy4r93", "gjnfkitemt93-", "db_trying", 3306,  NULL, MYSQLI_CLIENT_SSL);
+
+if (mysqli_connect_errno())
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+else {
+    "nice db is online";
+}
+
 $query = "SELECT * FROM user";
 $result = mysqli_query($conn, $query);
 while ($row = $result->fetch_assoc()) {
