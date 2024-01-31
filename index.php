6236a253700d3e423c7278ed4f916f68ba0e6d47
Dizzy4r <111083740+Dizzy4r@users.noreply.github.com>
2024-01-31 21:02:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
-<?php
-include 'connect.php';
-?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,20 +9,8 @@ include 'connect.php';
 </head>
 <body>
 
-<?php
 
-echo "e be the first PHP echo<br>";
-echo "This will be the seoncd";
-
-$query = "SELECT * FROM xxx";
-$result = mysqli_query($conn, $query);
-while ($row = $result->fetch_assoc()) {
-    echo $row['fld_name'];
-    echo "<br>";
-}
-
-?>
-
+    
 </body>
 </html>
 
