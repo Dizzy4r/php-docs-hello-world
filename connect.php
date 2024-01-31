56624a7ab7b7ce9be1812aba989b6929a929a090
Dizzy4r <111083740+Dizzy4r@users.noreply.github.com>
2024-01-31 18:01:57 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,10 @@
 <?php
+/*
 $conn = mysqli_init();
 mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, "tryingagain.mysql.database.azure.com", "dizzy4r93", "gjnfkitemt93-", "db_trying", 3306, NULL, MYSQLI_CLIENT_SSL);
+*/
 
+$conn = mysqli_connect("tryingagain.mysql.database.azure.com", "dizzy4r93", "gjnfkitemt93-", "db_trying", 3306);
 
 ?>
