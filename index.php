ced4460a9476cc7c1cef5db2d9eebf95b1297a28
Dizzy4r <111083740+Dizzy4r@users.noreply.github.com>
2024-01-31 17:50:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,18 +10,6 @@
 
 <?php
 
-$conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, "tryingagain.mysql.database.azure.com", "dizzy4r93", "gjnfkitemt93-", "db_trying", 3306, MYSQLI_CLIENT_SSL);
-
-if (mysqli_connect_errno())
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
-else {
-    "nice db is online";
-}
-
 echo "e be the first PHP echo<br>";
 echo "This will be the seoncd";
 
@@ -31,8 +19,6 @@ while ($row = $result->fetch_assoc()) {
     echo $row['fld_name'];
     echo "<br>";
 }
-
-mysqli_close($conn);
 
 ?>
 
