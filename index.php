014745ba29eda9f461444df801ad2498bff563d5
Dizzy4r <111083740+Dizzy4r@users.noreply.github.com>
2024-02-01 18:41:38 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,10 +9,6 @@
 <body>
 
 <?php
-include 'header.php';
-?>
-
-<?php
 
 $conn = mysqli_init();
 mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
@@ -22,9 +18,20 @@ if (mysqli_connect_errno())
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-?>
-
     
+?>
+    
+<header>tryingAgain.</header>
+
+<nav>
+    <div id="welcome">Welcome</div>
+    <div id="play"><a href="play.html">Play</a></div>
+    <div id="create"><a href="#">Create post</a></div>
+    <div id="contact"><a href="#">Contact</a></div>
+    <div id="home"><a href="#">Home</a></div>
+    <div id="log"><a href="#">Log in</a></div>
+</nav>
+
 <div id="cont" style="height:120vh;"></div>
     
 </body>
