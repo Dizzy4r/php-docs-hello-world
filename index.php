b9c86f1611c909584965d5b77efd3c1f85910107
Dizzy4r <111083740+Dizzy4r@users.noreply.github.com>
2024-02-01 18:23:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,18 +7,11 @@
     <title>tryingAgain</title>
 </head>
 <body>
+
+<?php
+include 'header.php';
+?>
     
-<header>tryingAgain.</header>
-
-<nav>
-    <div id="welcome">Welcome</div>
-    <div id="play"><a href="play.html">Play</a></div>
-    <div id="create"><a href="#">Create post</a></div>
-    <div id="contact"><a href="#">Contact</a></div>
-    <div id="home"><a href="#">Home</a></div>
-    <div id="log"><a href="#">Log in</a></div>
-</nav>
-
 <div id="cont" style="height:120vh;"></div>
     
 </body>
