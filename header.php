d45337bd967d2e9d95863d79cd370f29d0d9b1d7
Dizzy4r <111083740+Dizzy4r@users.noreply.github.com>
2024-02-01 18:22:45 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,1 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>tryingAgain</title>
+</head>
+<body>
+    
+<header>tryingAgain.</header>
 
+<nav>
+    <div id="welcome">Welcome</div>
+    <div id="play"><a href="play.html">Play</a></div>
+    <div id="create"><a href="#">Create post</a></div>
+    <div id="contact"><a href="#">Contact</a></div>
+    <div id="home"><a href="#">Home</a></div>
+    <div id="log"><a href="#">Log in</a></div>
+</nav>
+
+<div id="cont" style="height:120vh;"></div>
+    
+</body>
+</html>
