9cd2bcb4a6622aa889896d975fdfc25cc897e8c4
Dizzy4r <111083740+Dizzy4r@users.noreply.github.com>
2024-02-02 17:32:43 +0100

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tryingAgain</title>
+</head>
+<body>
+    
+<?php
+if(isset($_SESSION['login'])) {
+    session_destroy();
+    session_unset();
+    header('Location: index.php');
+}
+else {
+    header('Location: index.php');
+}
+?>
+
+</body>
+</html>
