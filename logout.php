69b2396c1a60cf5bfa0ca06146a7f6b57da7f4fd
Dizzy4r <111083740+Dizzy4r@users.noreply.github.com>
2024-02-03 14:33:40 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -8,13 +8,26 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>tryingAgain</title>
 </head>
-<body>
+<body id="bodd">
+
+<script>
+const bod = document.getElementById("bodd");
+function myScript() {
+    return {
+        </script>
+        <?php
+            session_destroy();
+            session_unset();
+            header('Location: index.php');
+        ?>
+        <script>
+    }
+}
+bod.addEventListener("load", myScript);
+
+</script>
     
-<?php
-    session_destroy();
-    session_unset();
-    header('Location: index.php');
-?>
+
 
 </body>
 </html>
