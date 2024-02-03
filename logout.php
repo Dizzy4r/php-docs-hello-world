16a88dc5196a4da40200d047806cbfb8162621f8
Dizzy4r <111083740+Dizzy4r@users.noreply.github.com>
2024-02-03 14:43:08 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,23 +1,6 @@
 <?php
 session_start();
+// unset($_SESSION['name']);
+session_destroy();
+header("location: index.php");
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>tryingAgain</title>
-</head>
-<body id="bodd">
-
-
-        <?php
-            session_destroy();
-            session_unset();
-            header('Location: index.php');
-        ?>
-
-<a href"index.php"> You have logged out. Press me to go to the main page.</a>
-    
-</body>
-</html>
