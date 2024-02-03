14dd8459c10d511e2b9423bac695b89850531e0f
Dizzy4r <111083740+Dizzy4r@users.noreply.github.com>
2024-02-03 14:06:34 +0100

    Update header.php

--- header.php
+++ header.php
@@ -25,7 +25,7 @@ if(isset($_SESSION['name'])) {
     echo "<div id='login'><a href='logout.php'>Log out</a></div>";
 }
 if(!isset($_SESSION['name'])) {
-    echo "<a href='login.php'>Log in</a>";
+    echo "<div id='loginbtn'><a href='login.php'>Log in</a></div>";
 }
 ?>
     </div>
