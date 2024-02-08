1981a146e7389b5cbbfe98b386447d702afa560a
Dizzy4r <111083740+Dizzy4r@users.noreply.github.com>
2024-02-08 21:39:02 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
+include 'loginscript.php';
 include 'connect.php';
+include 'header.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -29,25 +31,6 @@ include 'connect.php';
     }
 </style>
 
-    
-<h1>trYingAgain.</h1>
-
-<nav>
-    <div id="welcome">Welcome <?php if(isset($_SESSION['name'])) {
-            echo $_SESSION['name'];
-        }?> </div>
-    <div id="create"><a href="post.php">Post</a></div>
-    <div id="home"><a href="index.php">Home</a></div>
-        <?php
-if(isset($_SESSION['name'])) {
-    echo "<div id='login'><a href='logout.php'>Log out</a></div>";
-}
-if(!isset($_SESSION['name'])) {
-    echo "<div id='loginbtn'><a href='login.php'>Log in</a></div>";
-}
-?>
-</nav>
-
 <div id="xy">
 <p id="ppp"> Dont have an account? Create your user <a href="#"> here </a> </p>
     <br>
@@ -58,46 +41,6 @@ if(!isset($_SESSION['name'])) {
             <br>
         <input type="submit" name="sub2" value="Login" id="sub2">
 </form>
-</div>
-
-
-
-<p id="pen">
-<?php // LOGIN SECURE PHP
-
-if(isset($_POST['username'], $_POST['password'])) {
-    if(strlen($_POST['username']) < 5 || strlen($_POST['password'] < 5)) {
-        echo "Username and password must be above 5 characters.";
-        exit();
-    //    header('Location: login.php');
-    }
-}
-
-if(!isset($_POST['username'], $_POST['password'])) {
-    exit("Please fill both fields.");
-  //  header('Location: login.php');
-}
-
-if($stmt = $conn->prepare('SELECT fld_id, fld_password FROM userr WHERE fld_name = ?')) {
-            $name = htmlspecialchars($_POST['username']);
-            $stmt->bind_param('s', $name);
-            $stmt->execute();
-            $stmt->store_result();
-                if($stmt->num_rows > 0) {
-                    $stmt->bind_result($id, $password);
-                    $stmt->fetch();
-                        if($_POST['password'] === $password) {
-                            $_SESSION['name'] = $_POST['username'];
-                            header('Location: index.php');                           
-                            // session_regenerate_id();
-                           // $_SESSION['id'] = $id;
-                           // $_SESSION['login'] = $name;
-                        }
-                    } 
-            $stmt->close();
-        }
-?>
-</p>
 </div>
 
 
