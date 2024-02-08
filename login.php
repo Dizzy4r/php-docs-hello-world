b54fa9d69a9c65068cceb24b279da6371512eda9
Dizzy4r <111083740+Dizzy4r@users.noreply.github.com>
2024-02-08 21:12:38 +0100

    Update login.php

--- login.php
+++ login.php
@@ -47,35 +47,27 @@ include 'connect.php';
 <p id="pen">
 <?php // LOGIN SECURE PHP
 
-function val($x) { // Alters the submitted text to prevent XSS.
-    $x = trim($x);
-    $x = htmlspecialchars($x);
-    return $x;
-}
-
 if(isset($_POST['username'], $_POST['password'])) {
     if(strlen($_POST['username']) < 5 || strlen($_POST['password'] < 5)) {
         echo "Username and password must be above 5 characters.";
         exit();
-        header('Location: login.php');
+    //    header('Location: login.php');
     }
 }
 
 if(!isset($_POST['username'], $_POST['password'])) {
     exit("Please fill both fields.");
-    header('Location: login.php');
+  //  header('Location: login.php');
 }
 
 if($stmt = $conn->prepare('SELECT fld_id, fld_password FROM userr WHERE fld_name = ?')) {
-            $name = val($_POST['username']);
+            $name = htmlspecialchars($_POST['username']);
             $stmt->bind_param('s', $name);
             $stmt->execute();
             $stmt->store_result();
-        
                 if($stmt->num_rows > 0) {
                     $stmt->bind_result($id, $password);
                     $stmt->fetch();
-                    
                         if($_POST['password'] === $password) {
                             $_SESSION['name'] = $_POST['username'];
                             header('Location: index.php');                           
