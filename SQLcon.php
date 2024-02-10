de7e26f1c5f72095008cb01a5f8a1a0b1236fed9
Dizzy4r <111083740+Dizzy4r@users.noreply.github.com>
2024-02-10 18:16:13 +0100

    Update SQLcon.php

--- SQLcon.php
+++ SQLcon.php
@@ -1,7 +1,7 @@
 <?php
 try {
-    $conn = new PDO("sqlsrv:server = tcp:tryingless.database.windows.net,1433; Database = DB_tryingLess", "iAmRobertmor", "lolHax1993");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $connn = new PDO("sqlsrv:server = tcp:tryingless.database.windows.net,1433; Database = DB_tryingLess", "iAmRobertmor", "lolHax1993");
+    $connn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
@@ -9,5 +9,5 @@ catch (PDOException $e) {
 }
 $connectionInfo = array("UID" => "iAmRobertmor", "pwd" => "lolHax1993", "Database" => "DB_tryingLess", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:tryingless.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+$connn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
