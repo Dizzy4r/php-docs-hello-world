fbaffbdf272a3d3cb91edd411765929cdef8cfd7
Dizzy4r <111083740+Dizzy4r@users.noreply.github.com>
2024-02-11 20:47:32 +0100

    Update header.php

--- header.php
+++ header.php
@@ -11,18 +11,17 @@
 <h1>tryingAgain.</h1>
 
 <nav>
-    <div id="welcome">Welcome <?php if(isset($_SESSION['name'])) {
-            echo $_SESSION['name'];
-        }?> </div>
+    <div id="welcome">Welcome</div>
     <div id="create"><a href="post.php">Post</a></div>
     <div id="home"><a href="index.php">Home</a></div>
-        <?php
+    <div id='loginbtn'><a href='login.php'>Log in</a></div>
+        <?php /*
 if(isset($_SESSION['name'])) {
     echo "<div id='login'><a href='logout.php'>Log out</a></div>";
 }
 if(!isset($_SESSION['name'])) {
     echo "<div id='loginbtn'><a href='login.php'>Log in</a></div>";
-}
+}*/
 ?>
 </nav>
     
