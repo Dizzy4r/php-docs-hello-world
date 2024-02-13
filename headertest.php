6bd7ce63662474984c9f7d5912b09da038fd49d9
Dizzy4r <111083740+Dizzy4r@users.noreply.github.com>
2024-02-13 18:25:55 +0100

    Create headertest.php

---
+++ headertest.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>tryingAgain</title>
+</head>
+<body>
+    
+<h1>tryingAgain.</h1>
+
+<nav>
+    <div id="welcome">Welcome 
+<?php 
+if(isset($_SESSION['name'])) {
+    echo $_SESSION['name'];
+}
+?>
+</div>
+    <div id="create"><a href="post.php">Post</a></div>
+    <div id="home"><a href="index.php">Home</a></div>
+        <?php 
+if(isset($_SESSION['name'])) {
+    echo "<div id='login'><a href='logout.php'>Log out</a></div>";
+}
+if(!isset($_SESSION['name'])) {
+    echo "<div id='loginbtn'><a href='login.php'>Log in</a></div>";
+}
+?>
+</nav>
+    
