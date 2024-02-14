13608d5d4a653e969e8310d706b815d117a014c6
Dizzy4r <111083740+Dizzy4r@users.noreply.github.com>
2024-02-14 20:52:22 +0100

    Update random.php

--- random.php
+++ random.php
@@ -15,8 +15,41 @@ else {
   echo 'ikkje online';
 }
 ?>
+<style>
+#con {
+    width: 10vw;
+    height: 10vh;
+    background: pink;
+    margin-left: 45vw;
+    margin-top:10vh;
+    opacity: 0.0;
+    animation: 2s starteren cubic-bezier(0.215, 0.610, 0.355, 1) forwards 1s;
+}
 
-<h1> Dette er en h1 </h1>
+@keyframes starteren {
+    from {
+    width: 10vw;
+    height: 10vh;
+    background: pink;
+    margin-left: 45vw;
+    margin-top:45vh;
+    opacity: 0.0;
+    }
+    to {
+    width: 80vw;
+    opacity: 1.0;
+    height: 70vh;
+    background: rgb(255, 242, 218);
+    margin-left: 10vw;
+    margin-top:15vh;
+    box-shadow: 0px 0px 90px 70px rgb(165, 165, 165);
+    }
+}
+</style>
+
+<div id="con">
+<p style="text-align: center;">Heihei</p>
+</div>
 
 </body>
 </html>
