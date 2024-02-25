765b1acd4c7028fff8ac812f567f616f1281c43e
Dizzy4r <111083740+Dizzy4r@users.noreply.github.com>
2024-02-25 16:52:51 +0100

    Update connect.php
    Added a github secret for database password.

--- connect.php
+++ connect.php
@@ -1,9 +1,8 @@
 <?php
 
+$psw = getenv("PASSWORDLOL");
 $conn = mysqli_init();
 mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, "tryingagain.mysql.database.azure.com", "dizzy4r93", "iWantMy-Ball", "db_trying", 3306, NULL, MYSQLI_CLIENT_SSL);
-
-
+mysqli_real_connect($conn, "tryingagain.mysql.database.azure.com", "dizzy4r93", $psw, "db_trying", 3306, NULL, MYSQLI_CLIENT_SSL);
 
 ?>
