53bc97440861857de6cf22e74fae8d9192d8f02c
Dizzy4r <111083740+Dizzy4r@users.noreply.github.com>
2024-02-26 13:01:35 +0100

    Update random.php

--- random.php
+++ random.php
@@ -37,12 +37,39 @@ else {
       opacity: 0.6;
     }
   }
+
+  #bottomcontainer {
+    animation: openbottom 3s 2s forwards;
+    text-align: center;
+    margin-left: 50vw;
+    width: 0vw;
+    height: 0vh;
+    background: red;
+  }
+  @keyframes openbottom {
+    from {
+      height: 0vh;
+      margin-left: 50vw;
+      width: 0vw;
+      opacity: 0.0;
+    }
+    to {
+      margin-left: 25vw;
+      width: 50vw;
+      height: 40vh;
+      opacity: 0.6;
+    }
+  }
+  
 </style>
 
 <div id="container">
-  <div id=""></div>
+  <p>Just some text in the top container</p>
 </div>
-
+<br>
+<div id="bottomcontainer">
+  <p>Just some text in the bottom container</p>
+</div>
 
 </body>
 </html>
