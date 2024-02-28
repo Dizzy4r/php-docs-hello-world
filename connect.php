ba46c5781c617442dea930ea17d0f43999bfaf3b
Dizzy4r <111083740+Dizzy4r@users.noreply.github.com>
2024-02-28 20:01:36 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -4,5 +4,5 @@ $psw = getenv("PASSWORD");
 $conn = mysqli_init();
 mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, "tryingagain.mysql.database.azure.com", "dizzy4r93", $psw, "db_trying", 3306, NULL, MYSQLI_CLIENT_SSL);
-
+echo $psw;
 ?>
